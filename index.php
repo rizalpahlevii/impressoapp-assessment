2ab9fa72c099fef4de2025ce53086313cedf6ad5
Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2022-08-11 07:22:44 +0700

    initial commit

--- index.php
+++ index.php
@@ -8,21 +8,21 @@ for ($i = 1; $i < 30; $i++) {
 
 echo 13 . " = " . f($a, 13);
 
-function f($a, $n)
+function f($a, $x)
 {
     $tempArray = $a;
-    if (!$n < $a[0]) {
-        if (!array_search($n, $a, true)) {
-            array_push($tempArray, $n);
+    if (!$x < $a[0]) {
+        if (!array_search($x, $a, true)) {
+            array_push($tempArray, $x);
         }
     }
-    $searchValue = array_search($n, $tempArray, true);
+    $searchValue = array_search($x, $tempArray, true);
     if ($searchValue !== null) {
         if ($searchValue < 1) {
             return -1;
         } else {
             sort($tempArray);
-            $resultIndex = array_search($n, $tempArray, true);
+            $resultIndex = array_search($x, $tempArray, true);
             $index = $resultIndex - 1;
             if ($index < 0) {
                 return -1;
